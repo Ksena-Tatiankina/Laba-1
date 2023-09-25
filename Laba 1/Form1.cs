bf923ae98d0e189b4204ee7d3447752d285df079
ksena20048993@gmail.com <ksena20048993@gmail.com>
2023-09-25 20:49:56 +0700

    Внесение данных

--- Laba 1/Form1.cs
+++ Laba 1/Form1.cs
@@ -20,9 +20,30 @@ namespace Laba_1
             
         }
 
+        List<Student> List_Students = new List<Student>();
+        int numbers_students = 0;
+
         private void button_Enter_Click(object sender, EventArgs e)
         {
+            if ((textBox1.Text == "") || (textBox2.Text == "") || (textBox3.Text == "") || (textBox4.Text == ""))
+                MessageBox.Show("Введены не все данные");
+            else
+            {
+                int year = Convert.ToInt32(textBox3.Text);
+                int course = Convert.ToInt32(textBox4.Text);
+                //Student student = new Student(numbers_students, textBox1.Text, textBox2.Text, year, course)
+                List_Students.Add(new Student(numbers_students, textBox1.Text, textBox2.Text, year, course));
+                numbers_students++;
 
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+                textBox4.Clear();
+
+                MessageBox.Show("Данные внесены");
+            }
+                
+            
         }
     }
 }
