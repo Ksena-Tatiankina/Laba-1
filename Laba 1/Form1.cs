88039922f7df7b929a6c5054e708ce000c988493
ksena20048993@gmail.com <ksena20048993@gmail.com>
2023-10-10 23:00:39 +0700

    проверка введенных данных

--- Laba 1/Form1.cs
+++ Laba 1/Form1.cs
@@ -29,12 +29,60 @@ namespace Laba_1
                 MessageBox.Show("Введены не все данные");
             else
             {
+                if (int.TryParse(textBox_ID.Text, out int id))
+                {
+                    if (int.TryParse(textBox_year.Text, out int year))
+                    {
+                        if (int.TryParse(textBox_course.Text, out int course))
+                        {
+                            if (int.TryParse(textBox_Name.Text, out int name))
+                            {
+                                MessageBox.Show("Неверный формат значения 'ФИО'");
+                            }
+                            else
+                            {                                
+                                bool flag = true;
+                                for (int i = 0; i < numbers_students; i++)
+                                {
+                                    if (id == List_Students[i].ID)
+                                    {
+                                        flag = false;
+                                        MessageBox.Show("Введенное ID занято"); 
+                                        break;
+                                    }                                    
+                                }
+
+                                if (flag)
+                                {
+                                    List_Students.Add(new Student(id, textBox_Name.Text, textBox_major.Text, year, course));
+                                    numbers_students++;
+
+                                    textBox_ID.Clear();
+                                    textBox_Name.Clear();
+                                    textBox_major.Clear();
+                                    textBox_year.Clear();
+                                    textBox_course.Clear();
+
+                                    MessageBox.Show("Данные внесены");
+                                }
+                            }
+                        }
+                        else
+                            MessageBox.Show("Неверный формат значения 'курс' ");
+                    }
+                    else
+                        MessageBox.Show("Неверный формат значения 'год поступления' ");
+                }
+                else
+                    MessageBox.Show("Неверный формат значения 'ID'");
+
                 //добавление нового студента
-                int id = Convert.ToInt32(textBox_ID.Text);
-                int year = Convert.ToInt32(textBox_year.Text);
-                int course = Convert.ToInt32(textBox_course.Text);
-                
-                List_Students.Add(new Student(id, textBox_Name.Text, textBox_major.Text, year, course));
+                //int id = Convert.ToInt32(textBox_ID.Text);
+                //int year = Convert.ToInt32(textBox_year.Text);
+                //int course = Convert.ToInt32(textBox_course.Text);
+
+                //List_Students.Add(new Student(id, textBox_Name.Text, textBox_major.Text, year, course));
+                /*
                 numbers_students++;
 
                 textBox_ID.Clear();
@@ -44,70 +92,113 @@ namespace Laba_1
                 textBox_course.Clear();
 
                 MessageBox.Show("Данные внесены");
+                */
             }
         }
 
         //закрытие летней сессии
         private void button_Summer_Click(object sender, EventArgs e)
         {
-            //int currentID = Convert.ToInt32(textBox_ID_Find.Text); //запись текущего id
             if (textBox_ID_Find.Text == "")
                 MessageBox.Show("Введите ID студента");
             else
             {
-                int currentID = Convert.ToInt32(textBox_ID_Find.Text); //запись текущего id
+                //int currentID = Convert.ToInt32(textBox_ID_Find.Text); //запись текущего id
                 //поиск студента по id
-                for (int i = 0; i < numbers_students; i++)
+                if (int.TryParse(textBox_ID_Find.Text, out int currentID))
                 {
-                    if (currentID == List_Students[i].ID)
+                    for (int i = 0; i < numbers_students; i++)
                     {
-                        MessageBox.Show(List_Students[i].PassSession("Летняя")); //вызов метода
-                        break;
-                    }
-                    if (i == numbers_students - 1)
-                    {
-                        MessageBox.Show("ID не найдено");
+                        if (currentID == List_Students[i].ID)
+                        {
+                            MessageBox.Show(List_Students[i].PassSession("Летняя")); //вызов метода
+                            break;
+                        }
+                        if (i == numbers_students - 1)
+                        {
+                            MessageBox.Show("ID не найдено");
+                        }
                     }
                 }
+                else
+                    MessageBox.Show("Неверный формат ID");
             }            
         }
 
         //закрытие зимней сессии
         private void button1_Click(object sender, EventArgs e)
         {
-            //int currentID = Convert.ToInt32(textBox_ID_Find.Text); //запись текущего id
             if (textBox_ID_Find.Text == "")
                 MessageBox.Show("Введите ID студента");
             else
             {
-                int currentID = Convert.ToInt32(textBox_ID_Find.Text); //запись текущего id
-                if (numbers_students == 0)
-                    MessageBox.Show("ID не найдено");
-                //поиск студента по id
-                for (int i = 0; i < numbers_students; i++)
+                //int currentID = Convert.ToInt32(textBox_ID_Find.Text); //запись текущего id
+                if (int.TryParse(textBox_ID_Find.Text, out int currentID))
                 {
-                    if (currentID == List_Students[i].ID)
-                    {
-                        MessageBox.Show(List_Students[i].PassSession("Зимняя")); //вызов метода
-                        break;
-                    }
-                    if (i == numbers_students - 1)
-                    {
+                    if (numbers_students == 0)
                         MessageBox.Show("ID не найдено");
+                    //поиск студента по id
+                    for (int i = 0; i < numbers_students; i++)
+                    {
+                        if (currentID == List_Students[i].ID)
+                        {
+                            MessageBox.Show(List_Students[i].PassSession("Зимняя")); //вызов метода
+                            break;
+                        }
+                        if (i == numbers_students - 1)
+                        {
+                            MessageBox.Show("ID не найдено");
+                        }
                     }
                 }
+                else
+                    MessageBox.Show("Неверный формат ID");
             }
         }
 
         //получение полной информации
         private void button_full_Inf_Click(object sender, EventArgs e)
         {
-            //int currentID = Convert.ToInt32(textBox_ID_Find.Text); //запись текущего id
             if (textBox_ID_Find.Text == "")
                 MessageBox.Show("Введите ID студента");
             else
             {
-                int currentID = Convert.ToInt32(textBox_ID_Find.Text); //запись текущего id
+                /*                 
+                int currentID = 0;
+                try
+                {
+                    currentID = Convert.ToInt32(textBox_ID_Find.Text); //запись текущего id
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show("Неверный формат ID");
+                }
+                */
+
+                if (int.TryParse(textBox_ID_Find.Text, out int currentID))
+                {
+                    if (numbers_students == 0)
+                        MessageBox.Show("ID не найдено");
+                    //поиск студента по id
+                    for (int i = 0; i < numbers_students; i++)
+                    {
+                        if (currentID == List_Students[i].ID)
+                        {
+                            MessageBox.Show(List_Students[i].GetStudentInf()); //вызов метода                       
+                            break;
+                        }
+                        if (i == numbers_students - 1)
+                        {
+                            MessageBox.Show("ID не найдено");
+                            break;
+                        }
+                    }
+                }
+                else
+                    MessageBox.Show("Неверный формат ID");
+
+                /*
+                //int currentID = Convert.ToInt32(textBox_ID_Find.Text); //запись текущего id
                 if (numbers_students == 0)
                     MessageBox.Show("ID не найдено");
                 //поиск студента по id
@@ -123,70 +214,77 @@ namespace Laba_1
                         MessageBox.Show("ID не найдено");
                         break;
                     }
-                }
+                }       */
             }
         }
 
         //получение ФИО
         private void button_FIO_Click(object sender, EventArgs e)
         {
-            //int currentID = Convert.ToInt32(textBox_ID_Find.Text);
             if (textBox_ID_Find.Text == "")
                 MessageBox.Show("Введите ID студента");
             else
             {
-                int currentID = Convert.ToInt32(textBox_ID_Find.Text); //запись текущего id
-                if (numbers_students == 0)
-                    MessageBox.Show("ID не найдено");
-
-                for (int i = 0; i < numbers_students; i++)
+                //int currentID = Convert.ToInt32(textBox_ID_Find.Text); //запись текущего id
+                if (int.TryParse(textBox_ID_Find.Text, out int currentID))
                 {
-                    if (currentID == List_Students[i].ID)
-                    {
-                        MessageBox.Show(List_Students[i].GetFullName());                        
-                        break;
-                    }
-                    if (i == numbers_students - 1)
-                    {
+                    if (numbers_students == 0)
                         MessageBox.Show("ID не найдено");
-                        break;
+
+                    for (int i = 0; i < numbers_students; i++)
+                    {
+                        if (currentID == List_Students[i].ID)
+                        {
+                            MessageBox.Show(List_Students[i].GetFullName());
+                            break;
+                        }
+                        if (i == numbers_students - 1)
+                        {
+                            MessageBox.Show("ID не найдено");
+                            break;
+                        }
                     }
                 }
+                else
+                    MessageBox.Show("Неверный формат ID");
             }
         }
 
         //отчисление студента
         private void button_Credit_Click(object sender, EventArgs e)
-        {
-            //int currentID = Convert.ToInt32(textBox_ID_Find.Text);            
+        {            
             if (textBox_ID_Find.Text == "")
                 MessageBox.Show("Введите ID студента");
             else
             {
-                int currentID = Convert.ToInt32(textBox_ID_Find.Text); //запись текущего id
-                if (numbers_students == 0)
-                    MessageBox.Show("ID не найдено");
-
-                for (int i = 0; i < numbers_students; i++)
+                //int currentID = Convert.ToInt32(textBox_ID_Find.Text); //запись текущего id
+                if (int.TryParse(textBox_ID_Find.Text, out int currentID))
                 {
-                    if (currentID == List_Students[i].ID)
-                    {
-                        MessageBox.Show(List_Students[i].Dedact());
-                        break;
-                    }
-                    if (i == numbers_students - 1)
-                    {
+                    if (numbers_students == 0)
                         MessageBox.Show("ID не найдено");
-                        break;
+
+                    for (int i = 0; i < numbers_students; i++)
+                    {
+                        if (currentID == List_Students[i].ID)
+                        {
+                            MessageBox.Show(List_Students[i].Dedact());
+                            break;
+                        }
+                        if (i == numbers_students - 1)
+                        {
+                            MessageBox.Show("ID не найдено");
+                            break;
+                        }
                     }
                 }
+                else
+                    MessageBox.Show("Неверный формат ID");
             }
         }
 
         //перевод студента
         private void button_transfer_Click(object sender, EventArgs e)
         {
-            //int currentID = Convert.ToInt32(textBox_ID_Find.Text);
             if (textBox_ID_Find.Text == "")
             {
                 MessageBox.Show("Введите ID студента");
@@ -198,23 +296,28 @@ namespace Laba_1
                     MessageBox.Show("Введите направление для перевода");
                 else
                 {
-                    int currentID = Convert.ToInt32(textBox_ID_Find.Text); //запись текущего id
-                    if (numbers_students == 0)
-                        MessageBox.Show("ID не найдено");
-
-                    for (int i = 0; i < numbers_students; i++)
+                    //int currentID = Convert.ToInt32(textBox_ID_Find.Text); //запись текущего id
+                    if (int.TryParse(textBox_ID_Find.Text, out int currentID))
                     {
-                        if (currentID == List_Students[i].ID)
-                        {
-                            MessageBox.Show(List_Students[i].ChangeMajor(textBox_transfer.Text));
-                            break;
-                        }
-                        if (i == numbers_students - 1)
-                        {
+                        if (numbers_students == 0)
                             MessageBox.Show("ID не найдено");
-                            break;
+
+                        for (int i = 0; i < numbers_students; i++)
+                        {
+                            if (currentID == List_Students[i].ID)
+                            {
+                                MessageBox.Show(List_Students[i].ChangeMajor(textBox_transfer.Text));
+                                break;
+                            }
+                            if (i == numbers_students - 1)
+                            {
+                                MessageBox.Show("ID не найдено");
+                                break;
+                            }
                         }
                     }
+                    else
+                        MessageBox.Show("Неверный формат ID");
                 }
             }
             textBox_transfer.Clear();
